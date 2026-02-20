fc210ab4af212d9e28db5b98df808bd3bf89a146
Wolf <jachpera2015@gmail.com>
2026-02-19 20:08:46 -0600

    Feat: Entidades Team, Player y Match agregadas

---
+++ Domain/Entities/Match.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Match
+    {
+        public Guid Id { get; private set; }
+        public Guid HomeTeamId { get; private set; }
+        public Guid AwayTeamId { get; private set; }
+        public DateTime MatchDate { get; private set; }
+        public bool IsPlayed { get; private set; }
+
+        private Match() { }
+
+        public Match(Guid homeTeamId, Guid awayTeamId, DateTime matchDate)
+        {
+            Id = Guid.NewGuid();
+            HomeTeamId = homeTeamId;
+            AwayTeamId = awayTeamId;
+            MatchDate = matchDate;
+            IsPlayed = false;
+        }
+
+        public void MarkAsPlayed()
+        {
+            IsPlayed = true;
+        }
+    }
+}
---
+++ Domain/Entities/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Player
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Guid TeamId { get; private set; }
+
+        private Player() { }
+
+        public Player(string name, Guid teamId)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            TeamId = teamId;
+        }
+    }
+}
