fa506905042ebae8f636d10e2c35d4a45a62238d
Wolf <jachpera2015@gmail.com>
2026-02-19 20:10:43 -0600

    Feat: Contratos IUnitOfWork e ITeamRepository definidos

---
+++ Application/Abstractions/ITeamRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Abstractions
+{
+    public interface ITeamRepository
+    {
+        Task AddAsync(Team team);
+        Task<Team?> GetByIdAsync(Guid id);
+    }
+}
