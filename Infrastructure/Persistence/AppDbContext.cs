1104ab2afe8887f3f0c10d446ff574c31a6f496a
Wolf <jachpera2015@gmail.com>
2026-02-19 20:03:41 -0600

    feat(infraestructura): implementar DbContext, UnitOfWork y repositorio de equipos

---
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Team> Teams => Set<Team>();
+        public DbSet<Player> Players => Set<Player>();
+        public DbSet<Match> Matches => Set<Match>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasKey(t => t.Id);
+            modelBuilder.Entity<Player>().HasKey(p => p.Id);
+            modelBuilder.Entity<Match>().HasKey(m => m.Id);
+
+            modelBuilder.Entity<Player>()
+                .HasOne<Team>()
+                .WithMany(t => t.Players)
+                .HasForeignKey(p => p.TeamId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Infrastructure/Repositories/TeamRepository.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Persistence;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Application.Abstractions;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public class TeamRepository : ITeamRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TeamRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Team team)
+        {
+            await _context.Teams.AddAsync(team);
+        }
+
+        public async Task<Team> GetByIdAsync(Guid id)
+        {
+            return await _context.Teams.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
